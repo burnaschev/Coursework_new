664553d206416d473c72a423111800657ccf1b95
burnaschev <informal201144@gmail.com>
2023-05-26 19:51:03 +0400

    added main file

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from utils import get_five_operations, sorted_date
 
-options = sorted_date('operations.json')
+option = sorted_date('operations.json')
 
-get_five_operations(options)
+get_five_operations(option)
